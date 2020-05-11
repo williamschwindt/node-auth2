8b9d37fc715d332d12f5a73fd7e0b800c3fade12
William Schwindt <williammschwindt@gmail.com>
2020-05-11 12:25:34 -0700

    server and database set up

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const server = express()
+const port = process.env.PORT || 6000
+
+server.use(express.json())
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: 'something went wrong'
+    })
+})
+
+server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`)
+})
---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done)
+      }
+    }
+  },
+};
